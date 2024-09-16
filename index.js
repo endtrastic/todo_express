128708c915d26e03d6b49b0644ab02f0142e762e
endtrastic <a.tuvikene@voco.ee>
2024-09-16 12:06:39 +0300

    Write data to file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { rejects } = require('assert')
 const express = require('express')
+const { read } = require('fs')
 const app = express()
 const path = require('path')
 
@@ -6,10 +8,45 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const readFile =  (filename) => {
+    return new Promise((resolve, reject) =>{
+        fs.readFile(filename, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+            const tasks = data.split("\n")
+            path.resolve(tasks)
+    });
+  })  
+}
+
 app.get('/', (req, res) => {
-    res.render('index')
+
+    readFile('./tasks')
+    .then(tasks => {
+        console.log(tasks)
+        res.render('index', { tasks: tasks })
+    })
+  })
+
+app.use(express.urlencoded({ extended: true}));
+
+app.post('/', (req, res) => {
+    readFile('/tasks')
+      .then(tasks => {
+        tasks.push(req.body.task)
+        const data = tasks.join("\n")
+        fs.writeFile('./tasks', data, (err) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+            res.redirect('/')
+      })
+    })
 })
 
 app.listen(3001, () => {
-    console.log('Starting the server at localhost:3001')
+    console.log('Starting the server at http://localhost:3001')
 }) 
