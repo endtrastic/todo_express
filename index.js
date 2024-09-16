8bb3eba88cca41891049f603e26e27912dc3e3af
endtrastic <a.tuvikene@voco.ee>
2024-09-16 12:16:02 +0300

    Delete single task data from file

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ const readFile =  (filename) => {
   })  
 }
 
+const writeFile = (filename, data) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filename, data, "utf-8", (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        resolve(true);
+      });
+    });
+  };
+
 app.get('/', (req, res) => {
 
     readFile('./tasks')
@@ -32,20 +44,42 @@ app.get('/', (req, res) => {
 
 app.use(express.urlencoded({ extended: true}));
 
-app.post('/', (req, res) => {
-    readFile('/tasks')
-      .then(tasks => {
-        tasks.push(req.body.task)
-        const data = tasks.join("\n")
-        fs.writeFile('./tasks', data, (err) => {
-            if (err) {
-                console.error(err)
-                return;
-            }
-            res.redirect('/')
-      })
-    })
-})
+app.post("/", (req, res) => {
+
+    readFile("./tasks.json")
+    .then((tasks) => {
+        let index;
+        if (tasks.length === 0) {
+          index = 0;
+        } else {
+          index = tasks[tasks.length - 1].id + 1;
+        }
+        const newTask = {
+          "id": index,
+          "task": req.body.task,
+        };
+        tasks.push(newTask);
+        data = JSON.stringify(tasks, null, 2);
+        writeFile("tasks.json", data)
+        res.redirect('/')
+      });
+    }) 
+
+app.get("/delete-task/:taskId", (req, res) => {
+    let deletedtaskId = parseInt(req.params.taskId);
+    readFile("./tasks.json")
+    .then((tasks) => {
+      tasks.forEach((task, index) => {
+        if (task.id === deletedtaskId) {
+          tasks.splice(index, 1);
+        }
+      });
+      data = JSON.stringify(tasks, null, 2);
+      writeFile("tasks.json", data);
+      res.redirect("/");
+    });
+  });
+  
 
 app.listen(3001, () => {
     console.log('Starting the server at http://localhost:3001')
